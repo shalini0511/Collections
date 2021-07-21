05337daf226e1e560109d733d01ee086a75b6e45
shalini <shalini.v@tvsnext.io>
2021-07-21 22:00:48 +0530

    [shalini]List

---
+++ Collections/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List.ListDemo();
+            List.UsingArray();
+            Console.ReadLine();
+        }
+    }
+}
