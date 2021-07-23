20b9a12721cf334ce51855220596bbf13ed8d5db
shalini <shalini.v@tvsnext.io>
2021-07-23 21:29:17 +0530

    [shalini]Avoid Duplicates enteries

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,39 +1,122 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
     class Program
     {
+
+        public static Dictionary<string, List<AddrBook>> addressBook = new Dictionary<string, List<AddrBook>>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Collections Demo Problem!!!");
-            Console.WriteLine("Enter the option ");
-            switch (Console.ReadLine())
-            {
-                case "1":
-                    List.ListDemo();
-                    break;
-                case "2":
-                    SortedList.SortedListDemo();
-                    break;
-                case "3":
-                    Dictionary.DictionaryDemo();
-                    break;
-                case "4":
-                
-                    Collections.AddressBook.GetCustomer();
-                    Collections.AddressBook.Modify();
-                    Collections.AddressBook.ListingPeople();
-                    Collections.AddressBook.RemovePeople();
-                    break;
-                default:
-                    Console.WriteLine("Enter a valid option");
-                    break;
 
+            Console.WriteLine("Welcome to Address Book System!");
+            Console.WriteLine("Enter the number of address books: ");
+            int noOfAddressBook = Convert.ToInt32(Console.ReadLine());
+            int noOfBooks = 0;
+            while (noOfBooks < noOfAddressBook)
+            {
+                Console.WriteLine("Enter the address book name : ");
+                string addressbookname = Console.ReadLine();
+                AddrBook addrBook = new AddrBook();
+                Console.WriteLine("Enter the no of contacts in the address book: ");
+                int noOfContact = Convert.ToInt32(Console.ReadLine());
+
+                while (noOfContact > 0)
+                {
+                    Console.WriteLine("Enter the details of contact to be added: ");
+
+                    Console.Write("Enter First Name: ");
+                    string FirstName = Console.ReadLine();
+
+                    Console.Write("Enter Last Name: ");
+                    string LastName = Console.ReadLine();
+
+                    Console.Write("Enter Phone Number: ");
+                    string PhoneNumber = Console.ReadLine();
+
+                    Console.Write("Enter Address : ");
+                    string Addresses = Console.ReadLine();
+
+                    Console.Write("Enter City : ");
+                    string City = Console.ReadLine();
+
+                    Console.Write("Enter State : ");
+                    string State = Console.ReadLine();
+
+                    Console.Write("Enter ZipCode: ");
+                    string ZipCode = Console.ReadLine();
+
+                    Console.Write("Enter EmailId: ");
+                    string EmailId = Console.ReadLine();
+                    addrBook.GetCustomer(FirstName, LastName, PhoneNumber, Addresses, City, State, ZipCode, EmailId);
+                    noOfContact--;
+                    Console.WriteLine(" ");
+                    addrBook.ListingPeople();
+                }
+                Console.WriteLine("1.To modify the details");
+                Console.WriteLine("2.To remove the details");
+                Console.WriteLine("3.Exit!");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        List.ListDemo();
+                        break;
+                    case "2":
+                        SortedList.SortedListDemo();
+                        break;
+                    case "3":
+                        Dictionary.DictionaryDemo();
+                        break;
+                    case "4":
+                
+                        Collections.AddressBook.GetCustomer();
+                        Collections.AddressBook.ListingPeople();
+                        break;
+                    case "5":
+                        Collections.AddressBook.GetCustomer();
+                        AddressBook.Modify();
+                        Collections.AddressBook.ListingPeople();
+                        break;
+                    case "6":
+                        Collections.AddressBook.GetCustomer();
+                        Collections.AddressBook.ListingPeople();
+                        break;
+                    case "7":
+
+                        Collections.AddressBook.GetCustomer();
+                        Collections.AddressBook.RemovePeople();
+                        break;
+                    default:
+                        Console.WriteLine("Enter a valid option");
+                        break;
+
+
+                }
+                if (addressBook.ContainsKey(addressbookname))
+                {
+                    Console.WriteLine("Existing address book name");
+                    return;
+                }
+                else
+                {
+                    addressBook.Add(addressbookname, addrBook.people);
+                }
+
+
+                noOfBooks++;
+            }
+
+            foreach (KeyValuePair<string, List<AddrBook>> addr in addressBook)
+            {
+                Console.WriteLine("The address Books are:{0}", addr.Key);
 
             }
 
-
         }
+
+
     }
+    
 }
